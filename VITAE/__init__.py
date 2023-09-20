81e3fdf165333cfefc5015442f4e995c2f688df6
jaydu1 <413075930@qq.com>
2023-09-20 18:50:38 -0400

    Update __init__.py

--- VITAE/__init__.py
+++ VITAE/__init__.py
@@ -3,11 +3,11 @@ __all__ = ['VITAE']
 from VITAE.VITAE import VITAE
 from VITAE.utils import load_data
 
-__author__ = "Jin-Hong Du, Tianyu Chen, and Jingshu Wang"
+__author__ = "Jin-Hong Du, Tianyu Chen, Ming Gao, and Jingshu Wang"
 __copyright__ = "Copyright 2020, The Trajectory Inference Project"
 
 __license__ = "MIT"
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __maintainer__ = "Jin-Hong Du, Tianyu Chen, and Jingshu Wang"
 __email__ = "jinhongd@andrew.cmu.edu"
 __status__ = "Development"
